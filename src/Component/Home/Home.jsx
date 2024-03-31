5abca0a1abbc503cc3b7fa2147cebfa63525ba9c
sourovDV <sourovchandra65@gmail.com>
2024-04-01 01:41:54 +0600

    Create a my portfolio website

---
+++ src/Component/Home/Home.jsx
@@ -0,0 +1,10 @@
+ 
+const Home = () => {
+    return (
+        <div>
+            <p>Hellow, everyOne, i am a root file, today, can i help you</p>
+        </div>
+    );
+};
+
+export default Home;
